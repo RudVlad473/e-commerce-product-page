f2eb26144196bc226b1d1274703d0f9e91ab1a0a
Rudenko Vladimir <72340706+RudVlad473@users.noreply.github.com>
2023-02-10 21:52:09 +0200

    made navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Navbar from "./components/Navbar/Navbar"
 import styles from "./App.module.scss"
+import Gallery from "./components/Gallery/Gallery"
 
 const App = () => {
   return (
     <div className={styles["main-grid"]}>
       <Navbar />
+      <Gallery />
     </div>
   )
 }
---
+++ src/components/Gallery/Gallery.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from "react"
+
+const Gallery: FC<{ picturesUrls: string[] }> = ({ picturesUrls }) => {
+  return <div></div>
+}
+
+export default Gallery
