1af498f381d169f91532ddcc5ca6b42e39f0c0b7
Rudenko Vladimir <72340706+RudVlad473@users.noreply.github.com>
2023-05-03 12:11:35 +0300

    fixed mobile design

--- src/components/Navlist/Navlist.tsx
+++ src/components/Navlist/Navlist.tsx
@@ -15,7 +15,8 @@ const Navlist: FC<NavlistProps> = ({ links, display = "HORIZONTAL" }) => {
     <ul
       className={classNames(styles["nav-list"], {
         [styles["nav-list--vertical"]]: display === "VERTICAL",
-      })}>
+      })}
+    >
       {links.map(({ displayName, link }) => (
         <li key={displayName}>
           <a href={link}>{displayName}</a>
